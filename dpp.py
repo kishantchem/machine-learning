5a5cff25fc003189a7a33017d79f75a94bc62fb0
kishantchem <kishant9528@gmail.com>
2024-12-15 22:36:26 +0530

    added python script for data PREPROCESSING

--- dpp.py
+++ dpp.py
@@ -1,2 +1,12 @@
 ### IMPORT REQUIRED PACKAGES ###
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+### USING MATPLOTLIB FOR PLOT ####
+
+radius = [1.0, 2.0, 3.0, 4.0]
+area = [3.14159, 12.56636, 28.27431, 50.26544]
+
+plt.plot(radius, area)
+plt.show()
